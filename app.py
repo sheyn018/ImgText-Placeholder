351060e77d0057d0ce8d9a0b3d07df731e16ab17
sheyn018 <sheane39@gmail.com>
2024-03-08 18:06:11 +0800

    fix

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import requests
 
 app = Flask(__name__)
 
 # Replace this with your actual Google Fonts API key
-google_fonts_api_key = "AIzaSyA2oLesrP-WPRZOSzQvb9IMQ5NYwytHfbA"
+google_fonts_api_key = ""
 google_fonts_api_url = f"https://www.googleapis.com/webfonts/v1/webfonts?key={google_fonts_api_key}"
 
 def fetch_google_fonts():
@@ -23,6 +23,15 @@ def home():
     default_font = "Playfair Display"
     default_text = default_font
     return render_template('index.html', text=default_text, font_type=default_font, fonts=fonts)
+
+@app.route('/save_image', methods=['POST'])
+def save_image():
+    if request.method == 'POST':
+        image_data = request.form.get('image_data')
+        # Process the image_data as needed (e.g., save to a file, database, etc.)
+        # For now, let's just print it
+        print("Received image data:", image_data)
+        return jsonify({'message': 'Image data received successfully'})
 
 if __name__ == '__main__':
     app.run(debug=True)
